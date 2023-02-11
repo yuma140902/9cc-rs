e4de3b7cd76030db8e4e6f5a2e087f1a69d3a5c1
yuma14 <yuma140902@gmail.com>
2023-02-12 02:49:07 +0900

    use anyhow

--- src/main.rs
+++ src/main.rs
@@ -1,36 +1,41 @@
-use ninecc::strtol;
+use anyhow::bail;
+use anyhow::Context as _;
 use std::env;
+use std::iter::Peekable;
 
-fn main() {
+fn strtol<I>(iter: &mut Peekable<I>) -> anyhow::Result<u32>
+where
+    I: Iterator<Item = char>,
+{
+    ninecc::strtol(iter).with_context(|| format!("invalid character: {:?}", iter.peek()))
+}
+
+fn main() -> anyhow::Result<()> {
     let args: Vec<_> = env::args().collect();
     let s = args.get(1).expect("Usage: 9ccrs INPUT");
 
-    println!(
-        "\
-.intel_syntax noprefix
-.globl main
-main:"
-    );
+    println!(".intel_syntax noprefix");
+    println!(".globl main");
+    println!("main:");
 
     let mut iter = s.chars().peekable();
 
-    println!("  mov rax, {}", strtol(&mut iter).unwrap());
+    println!("  mov rax, {}", strtol(&mut iter)?);
 
-    loop {
-        if let Some(c) = iter.peek() {
-            if *c == '+' {
-                iter.next();
-                println!("  add rax, {}", strtol(&mut iter).unwrap());
-                continue;
-            }
-            if *c == '-' {
-                iter.next();
-                println!("  sub rax, {}", strtol(&mut iter).unwrap());
-                continue;
-            }
-        }
-        break;
+    while let Some(c) = iter.peek() {
+        let op = if *c == '+' {
+            "add"
+        } else if *c == '-' {
+            "sub"
+        } else {
+            bail!("invalid character: {:?}", c);
+        };
+
+        iter.next();
+        println!("  {} rax, {}", op, strtol(&mut iter)?);
     }
 
     println!("  ret");
+
+    Ok(())
 }
