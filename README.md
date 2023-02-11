bfbb116fcd3c9ae9275ba99ff5d36490c7081de7
yuma14 <yuma140902@gmail.com>
2023-02-11 19:18:58 +0900

    init
    equivalent to https://github.com/rui314/9cc/commit/56e94442ae8844688d5390851e5b29ba0c946e2f

---
+++ README.md
@@ -0,0 +1,3 @@
+# 9cc-rs
+
+A rewrite of [9cc](https://github.com/rui314/9cc) in Rust.
---
+++ src/main.rs
@@ -0,0 +1,16 @@
+use std::env;
+
+fn main() {
+    let args: Vec<_> = env::args().collect();
+    let s = args.get(1).expect("Usage: 9ccrs INPUT");
+    let val: i32 = s.trim().parse().expect("error not a number");
+    println!(
+        "\
+.intel_syntax noprefix
+.global main
+main:
+\tmov rax, {}
+\tret",
+        val
+    );
+}
