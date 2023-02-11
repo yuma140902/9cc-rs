c27618f490063a0bbd2962a595e1f32f05ae590b
yuma14 <yuma140902@gmail.com>
2023-02-12 02:26:03 +0900

    add addition and subtraction

--- src/main.rs
+++ src/main.rs
@@ -1,16 +1,36 @@
+use ninecc::strtol;
 use std::env;
 
 fn main() {
     let args: Vec<_> = env::args().collect();
     let s = args.get(1).expect("Usage: 9ccrs INPUT");
-    let val: i32 = s.trim().parse().expect("error not a number");
+
     println!(
         "\
 .intel_syntax noprefix
 .globl main
-main:
-\tmov rax, {}
-\tret",
-        val
+main:"
     );
+
+    let mut iter = s.chars().peekable();
+
+    println!("  mov rax, {}", strtol(&mut iter).unwrap());
+
+    loop {
+        if let Some(c) = iter.peek() {
+            if *c == '+' {
+                iter.next();
+                println!("  add rax, {}", strtol(&mut iter).unwrap());
+                continue;
+            }
+            if *c == '-' {
+                iter.next();
+                println!("  sub rax, {}", strtol(&mut iter).unwrap());
+                continue;
+            }
+        }
+        break;
+    }
+
+    println!("  ret");
 }
